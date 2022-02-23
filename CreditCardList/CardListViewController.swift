7e1ea620300a8e9c8090dfd386f866a37fdaf61b
dorysojin <luv2hong@gmail.com>
2022-02-23 20:02:27 +0900

    TableCell 생성 후 UI 구성, 기본 List 설정

--- CreditCardList/CardListViewController.swift
+++ CreditCardList/CardListViewController.swift
@@ -6,7 +6,39 @@
 //
 
 import UIKit
+import Kingfisher
 
 class CardListViewController: UITableViewController {
+    // 전달 받을 데이터
+    var creditCardList: [CreditCard] = []
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // UITableView Cell Register
+        let nibName = UINib(nibName: "CardListCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return creditCardList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CardListCell", for: indexPath) as? CardListCell else { return UITableViewCell() }
+        
+        cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)위"
+        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
+        cell.cardNameLabel.text = "\(creditCardList[indexPath.row].name)"
+        
+        // kingfisher을 사용한 imgURL 데이터 전달
+        let imageURL = URL(string: creditCardList[indexPath.row].cardImageURL)
+        cell.cardImageView.kf.setImage(with: imageURL)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80 // Cell 높이 지정
+    }
 }
